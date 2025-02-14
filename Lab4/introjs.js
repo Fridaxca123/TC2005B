c8e72c662eb5584e64a21cd0ab20e4023327755d
Frida Xcaret Vargas Trejo <fridaxcaretvargastrejo@Fridas-MacBook-Air-5.local>
2025-02-13 18:04:24 -0600

    agregar nuevo

---
+++ Lab4/introjs.js
@@ -0,0 +1,11 @@
+let respuesta = confirm("¿Estás en la pagina correcta?");
+if (respuesta) {
+    alert("¡Bienvenido! 🎉");
+} else {
+    alert("Bye 👋 ");
+}
+console.warn("Esta es una advertencia");
+console.error("Hola error");
+
+
+
