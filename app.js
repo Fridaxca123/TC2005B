90ab354dc8e2577a9e10f5f2c5a2ac972d2f1bc7
Frida Xcaret Vargas Trejo <fridaxcaretvargastrejo@Fridas-MacBook-Air-5.local>
2025-02-27 10:59:58 -0600

    lab 12

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
--- route/personajes.routes.js
+++ route/personajes.routes.js
@@ -1,59 +1,26 @@
 const express = require('express');
+
 const router = express.Router();
-const html_header = `<!DOCTYPE html>
-<html>
-<head>
-    <title>Laboratorio 9: Frida</title>
-    <meta charset="UTF-8">
-    <style>
-        body {
-            background-color: #f1e2f5;
-            text-align: left;
-            font-family: monospace;
-            padding: 20px;
-            line-height: 1.6;
-        }
-        h1, h2 {
-            margin-bottom: 20px;
-        }
-        p {
-            margin-bottom: 15px;
-        }
-        aside {
-            float: right;
-            margin-left: 20px;
-        }
-        img {
-            border-radius: 50%;
-            width: 150px;
-            height: 150px;
-            object-fit: cover;
-        }
-    </style>
-</head>
-<body>`;
+
+const html_header = ``;
 
 const html_footer = `</body></html>`;
 
-const html_content_form = `
-    <h1>Bienvenido a ternushop!</h1>
-    <form method="POST">
-        <input type="text" name="personaje">
-        <button type="submit">Enviar</button>
-    </form>`;
+const html_content_form = ``;
 
 // Arreglo para almacenar personajes
 const Terurin = [];
 
 //Cuando se registra un middleware con app.get(), 
 //el middleware sólo se registra para el método HTTP GET
-router.get('/personajes/agregar', (request, response, next) => {
-    response.send(html_header + html_content_form + html_footer);
+router.get('/agregar', (request, response, next) => {
+    response.render('agregar_personaje')
+    //response.send(html_header + html_content_form + html_footer);
 });
 
 //Cuando se registra un middleware con app.post(), 
 //el middleware sólo se registra para el método HTTP POST
-router.post('/personajes/agregar', (request, response, next) => {
+router.post('/agregar', (request, response, next) => {
     console.log(request.body);
     Ternurin.push(request.body.nombre); // Asegúrate de que Terurin está definido antes.
 
@@ -68,6 +35,7 @@ router.post('/personajes/agregar', (request, response, next) => {
     html += html_footer;
 
     response.send(html);
+    respond.render('lista_personajes');
 });
 
 const path = require('path');
